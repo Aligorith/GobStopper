70616ab43f2118fbc4c03e223bef1b5f0264abe3
Joshua Leung <aligorith@gmail.com>
2019-08-23 23:56:29 +1200

    Stop every video element on each page visited
    Previously, only the first video element encountered would be muted.
    On YouTube there is only a single video element so this will be fine,
    but on other sites, this may not be the case. Loosen this restriction
    so that this addon will work on sites with multiple video elements
    (where the first one encountered may not actually be the one playing
    sounds).

--- src/content.js
+++ src/content.js
@@ -2,10 +2,10 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if (request.message === "stop_all_media") {
-			// TODO: Stop every video, not just the first one
-			var video = document.getElementsByTagName("video")[0];
-			if (!video.paused) {
-				video.pause();
+			for (let video of document.getElementsByTagName("video")) {
+				if (!video.paused) {
+					video.pause();
+				}
 			}
 		}
 	}
