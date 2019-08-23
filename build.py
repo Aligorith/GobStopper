f13a2ecd3f38f68545a0c9e885cc5ee70b772192
Joshua Leung <aligorith@gmail.com>
2019-08-24 01:53:59 +1200

    build.py - Beef up the buildscript to try to resolve issues during development
    * Read version string from manifest instead of requiring it to be
      hardcoded.
    
    * Pretty format the version string nicer (instead of just pasting it
      in and having it look bad).
    
    * Work around zip file locking by writing to a temp file and then
      trying to move the data to that file. Saves having to try several
      times if the previous build is already loaded for testing.

--- build.py
+++ build.py
@@ -4,14 +4,16 @@
 # in a single, easy to run operation that doesn't pollute the
 # src directory.
 
+import json
+import shutil
 import zipfile
 
 ##############################################
 # Settings - Edit This First
 
-# Version Number
-VERSION = None
-#VERSION = "0.1.0"
+# Include Version Number
+INCLUDE_VERSION = True
+#INCLUDE_VERSION = False
 
 # Targets - The output filenames to save to
 TARGETS = ["GobStopper{version_str}.xpi"]
@@ -28,17 +30,67 @@ SOURCES = [
 ]
 
 ##############################################
+# Utilities
+
+# Read version string from manifest
+# > returns: (str) Version number string
+def getVersionString():
+	if INCLUDE_VERSION:
+		# Read manifest to find the current version number
+		with open("src/manifest.json") as f:
+			manifest = json.load(f)
+		
+		# Read the version string
+		versionStr = manifest["version"]
+		
+		# Reformat it for use in a filename
+		# - Prefix with hypen
+		# - Use underscores instead of dots (so extensions don't get weird)
+		return "-%s" % (versionStr.replace('.', '_'))
+	else:
+		# Don't include a version number
+		return ""
+
+# Check if the given filename is writeable
+# < fileN: (str) Path to check
+# > returns: (bool) Whether file can be written to
+def probeFilenameWriteable(fileN):
+	try:
+		with open(fileN, 'w') as f:
+			return True
+		# TODO: return False otherwise?
+	except OsError:
+		return False
+
+# Write the files specified in SOURCES into a zip file
+# < fileN: (str) Name/path of zip file to write to
+def writeZipFile(fileN):
+	with zipfile.ZipFile(fileN, 'w') as zf:
+		for fileN in SOURCES:
+			path = SRC_ROOT + fileN
+			zf.write(path, fileN)
+
+# Force overwrite the dst file (in spite of Windows File Locks)
+# < src: (str) Temporary file name the data is currently stored in
+# < dst: (str) The filename that the ZIP file should actually have
+def replaceZipFile(*, src="tempName.zip", dst="realName.zip"):
+	print(f"Copying '{src}' to '{dst}'...")
+	shutil.move(src, dst)
+
+##############################################
 
 # Write zipfiles
-versionStr = f"{VERSION}" if VERSION is not None else ""
+versionStr = getVersionString()
 
 for targetName in TARGETS:
 	targetName = targetName.format(version_str=versionStr)
 	print(f"Creating {targetName}...")
 	
-	with zipfile.ZipFile(targetName, 'w') as zf:
-		for fileN in SOURCES:
-			path = SRC_ROOT + fileN
-			zf.write(path, fileN)
+	if probeFilenameWriteable(targetName):
+		writeZipFile(targetName)
+	else:
+		tempName = targetName + ".new.zip"
+		writeZipFile(tempName)
+		replaceZipFile(src=tempName, dst=targetName)
 
 input("Done! Press Enter to continue...")
